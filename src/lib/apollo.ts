ae9c886b2a369f59413438970496f6bd1c025e0d
Tiago Pires <tiago.pires3006@gmail.com>
2022-06-24 07:46:17 -0300

    Add subscriber function with useMutation from appolo

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4ojto8h08bs01xtc8llew8w/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_API_ACESS_TOKEN}`
+  },
   cache: new InMemoryCache()
 })
